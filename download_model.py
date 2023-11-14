66eb5d9ec72f2195ff47de6004efdee47a04cd08
Hafidh Soekma Ardiansyah <46504516+hafidh561@users.noreply.github.com>
2023-11-14 18:10:27 +0700

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,4 +1,4 @@
 import gdown
 
-url = "https://drive.google.com/u/4/uc?id=1MN-pDDl07iMwfB6BN4MIvMqs1mNElmgF"
+url = "https://drive.google.com/u/4/uc?id=1QAxtjwAe_lcRM75DeAlaUaQkGmcn2sNE"
 gdown.download(url)
